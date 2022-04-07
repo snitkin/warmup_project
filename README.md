ebadd24daec386ec00211763b4b0d49924000f1b
ubuntu <samuel.nitkin@gmail.com>
2022-04-07 21:29:54 +0000

    finished person follower, bags not working

--- README.md
+++ README.md
@@ -15,6 +15,25 @@ Drive in a suqare
     
     Challenges: As I begin this project, the biggest challenge is developing a workflow for this class. The speed of my VM and various issues trying to connect to the robot greatly slowdown the speed of my work. It was also challenging to understand that there had to be a pause before the topic was published, I only figured this out when the labsolutions were posted. Additionally, there seemed to be error in the robot turning 90 degrees on subsequent turns, and the source of that error is unknown.
 
+Person Follower 
+    High level description:
+        robot rotates towards nearest object and moves towards it proportionally
 
+    Code Explanation:
+        The code uses object oriented programming to subscribe to laser scanner with a call back function and publish to command velocity. In the call back function, it takes the array of distances and sets 0 to a larger number to ignore the 0s that represent no object. It takes the min distance as the distance to the person and the and the argmin as the angle to the person and moves towards the person with a speed and angular velocity proportional to the angle and the distance- a nearest distance the robot is willing to get
+        
+    GIF: ![gif of person follower](gifs/person_follower.gif)
+
+    Challenges: Knowing the proportions to set the speed to is difficult and takes some trial and error. However my main conceptual difficulty is thinking about how to aggregate the sensor readings to account for noise
+
+Wall Follower
+    High level description:
+
+    Code Explanation:
+
+    GIF:
+
+    Challenges:
+    
 Futrue Work:
 Takeaways: 
---
+++ scripts/person_follower.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import rospy
+import numpy as np
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist, Vector3
+
+#set global variable for minimum distance
+minDist = 0.4
+
+
+class Follower:
+
+        def __init__(self):
+
+                # subscribe to the robot's RGB camera data stream
+                self.image_sub = rospy.Subscriber('scan',
+                        LaserScan, self.laser_callback)
+                
+                # setup publisher to the cmd_vel ROS topic
+                self.robot_movement_pub = rospy.Publisher('cmd_vel', 
+                        Twist, queue_size=10)
+
+        def laser_callback(self, data):
+
+            myArr = np.array(data.ranges)
+
+            #if value is 0, set to nan
+            myArr[myArr == 0] = 60
+            #get the angle and the distance of the closest object
+            closeAngle = np.argmin(myArr)
+            closeDist = np.min(myArr) - minDist
+
+            myLinear = Vector3(.5 * closeDist, 0, 0)
+
+            if closeAngle <= 180:
+                #if angle is in the first 180 degrees, turn that way
+                angProp = closeAngle
+            else: 
+                #if angle is in the next 180, we want to turn the other way,
+                #so negative rotation
+                angProp = (closeAngle - 360)
+            
+            #proportional angular speed, largest is 180 so
+            #.01 makes sense, don't want robot going faster than 2
+            myAngular = Vector3(0, 0, .01 * angProp)
+
+            my_twist = Twist(
+                #move forward at constant speed
+                linear = myLinear,
+                angular = myAngular
+            )
+            # allow the publisher enough time to set up before publishing the first msg
+            #rospy.sleep(0.5)
+
+            # publish the message
+            self.robot_movement_pub.publish(my_twist)
+
+        def run(self):
+            rospy.spin()
+                
+if __name__ == '__main__':
+        #initialize node and run code
+        rospy.init_node('person_follower')
+        follower = Follower()
+        follower.run()
